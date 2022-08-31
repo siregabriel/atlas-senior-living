f3c32a95ab85ba5a332340b45ab114d69a7fd36d
Gabriel Rosales Montes <grosales@atlasseniorliving.com>
2022-08-31 15:28:55 -0500

    add autodate javascript file

---
+++ footer-main-site/autodate.js
@@ -0,0 +1,6 @@
+
+         //auto actualizacion de año con javascript
+        const d = new Date();
+        let year = d.getFullYear();
+        document.getElementById("date").innerHTML = year;
+        //debe de correr despues del ID indicado
